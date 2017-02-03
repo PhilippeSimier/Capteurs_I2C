5412f723ae8b50d24880a50dce3e3ec8228592bd
PhilippeSimier <philaure@wanadoo.fr>
2017-02-03 10:58:23 +0100

    class c++ i2c

---
+++ i2c/i2c.cpp
@@ -0,0 +1,85 @@
+#include "i2c.h"
+
+
+    i2c::i2c(int adresseI2C){
+        SetupInterface("/dev/i2c-1",adresseI2C);
+    }
+
+
+
+    void i2c::SetupInterface (const char *device, int devId){
+        if ((fd = open (device, O_RDWR)) < 0)
+            cout << "Unable to open I2C device:" << endl;
+
+        if (ioctl (fd, I2C_SLAVE, devId) < 0)
+            cout << "Unable to select I2C device:" << endl ;
+
+    }
+
+    int i2c::i2c_smbus_access (int fd, char rw, uint8_t command, int size, union i2c_smbus_data *data)
+    {
+        struct i2c_smbus_ioctl_data args ;
+
+        args.read_write = rw ;
+        args.command    = command ;
+        args.size       = size ;
+        args.data       = data ;
+        return ioctl (fd, I2C_SMBUS, &args) ;
+    }
+
+
+    unsigned char i2c::Read(){
+        union i2c_smbus_data data ;
+
+        if (i2c_smbus_access (fd, I2C_SMBUS_READ, 0, I2C_SMBUS_BYTE, &data))
+            return -1 ;
+        else
+            return data.byte & 0xFF ;
+
+    }
+
+    unsigned char i2c::ReadReg8 (int reg){
+         union i2c_smbus_data data;
+
+        if (i2c_smbus_access (fd, I2C_SMBUS_READ, reg, I2C_SMBUS_BYTE_DATA, &data))
+            return -1 ;
+        else
+            return data.byte & 0xFF ;
+
+    }
+
+    unsigned short i2c::ReadReg16 (int reg){
+        union i2c_smbus_data data;
+
+        if (i2c_smbus_access (fd, I2C_SMBUS_READ, reg, I2C_SMBUS_WORD_DATA, &data))
+            return -1 ;
+        else
+            return data.word & 0xFFFF ;
+    }
+
+    unsigned char i2c::Write (int data){
+        return i2c_smbus_access (fd, I2C_SMBUS_WRITE, data, I2C_SMBUS_BYTE, NULL) ;
+
+    }
+
+    unsigned char i2c::WriteReg8 (int reg, int value){
+        union i2c_smbus_data data ;
+
+        data.byte = value ;
+        return i2c_smbus_access (fd, I2C_SMBUS_WRITE, reg, I2C_SMBUS_BYTE_DATA, &data) ;
+
+    }
+
+    unsigned short i2c::WriteReg16 (int fd, int reg, int value){
+        union i2c_smbus_data data ;
+
+        data.word = value ;
+        return i2c_smbus_access (fd, I2C_SMBUS_WRITE, reg, I2C_SMBUS_WORD_DATA, &data) ;
+
+    }
+
+
+
+
+
+
---
+++ i2c/main.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+*    test de la classe i2C avec un composant DS1620
+*    Affiche la température en °C
+*    g++ main.cpp i2c.cpp -o test
+*************************************************************************/
+
+
+#include <iostream>
+#include "i2c.h"
+
+using namespace std;
+
+int main()
+{
+    i2c Ds1620(0x4F);  // création d'un objet DS1620
+
+    short valeur, data, msb, lsb;
+    char etat;
+
+    Ds1620.Write(0x51);  // demarrage de la conversion
+    do{
+	etat = Ds1620.ReadReg8(0xAC);
+        etat = etat & 0x80;
+//        sleep(1);
+    } while (etat != 0x80);
+
+    valeur = Ds1620.ReadReg16(0xAA);
+    msb = (valeur & 0x00FF) << 8;
+    lsb = (valeur & 0xFF00) >> 8;
+    data = msb | lsb;
+    cout << (float)data/256 << endl;
+
+    return 0;
+}
