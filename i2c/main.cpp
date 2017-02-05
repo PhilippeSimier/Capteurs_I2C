dcc85c2651dd0aee7889ed6263801e1e3e74b90d
PhilippeSimier <philaure@wanadoo.fr>
2017-02-05 22:54:10 +0100

    update

--- i2c/main.cpp
+++ i2c/main.cpp
@@ -13,6 +13,8 @@ using namespace std;
 int main()
 {
     i2c Ds1620(0x4F);  // création d'un objet DS1620 à l'adresse 4F
+    int tab[32]={0};
+    int val, i;
 
     short valeur, data;
     char etat;
@@ -24,7 +26,15 @@ int main()
 
     valeur = Ds1620.ReadReg16(0xAA);
     data = ((valeur & 0x00FF) << 8) | ((valeur & 0xFF00) >> 8);  // permutation des octets
-    cout << (float)data/256 << endl;
+    cout << (float)data/256 << endl;   // affichage de la temperature
 
+    val = Ds1620.ReadBlockData (0xAA, 4, tab);  // test de la lecture d'un bloc de donnée
+    cout << val << endl;    // affiche le nombre de donnée lue
+    if (val !=0){
+	for(i = 0; i< val; i++){
+	   cout << " " << tab[i];
+	}
+    cout << endl;
+    }
     return 0;
 }
