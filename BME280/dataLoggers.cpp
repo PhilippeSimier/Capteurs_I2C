1954a86158a7b4cf5ee60ba5defcdd58f9672223
PhilippeSimier <philaure@wanadoo.fr>
2017-02-25 17:46:43 +0100

    ajout point de rosée

--- BME280/dataLoggers.cpp
+++ BME280/dataLoggers.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
 
     capteur.donnerAltitude(74.0);   // initialisation de l'altitude du capteur
     driver = get_driver_instance();
-    connection = driver -> connect("localhost", "root", "********");
+    connection = driver -> connect("localhost", "root", "jelbroet");
 
     stmt = connection->createStatement();
 
@@ -31,11 +31,11 @@ int main(int argc, char* argv[]) {
     stmt->execute("USE Valeurs");
 
     //creation de la table dataBme280 si elle n'existe pas
-    stmt->execute("CREATE TABLE IF NOT EXISTS dataBme280 ( id int(11) NOT NULL AUTO_INCREMENT, temperature float NOT NULL, pression float NOT NULL, humidite float NOT NULL, date datetime NOT NULL, PRIMARY KEY (id))");
+    stmt->execute("CREATE TABLE IF NOT EXISTS dataBme280 ( id int(11) NOT NULL AUTO_INCREMENT, temperature float NOT NULL, pression float NOT NULL, humidite float NOT NULL, pointDeRosee float NOT NULL, date datetime NOT NULL, PRIMARY KEY (id))");
 
-    // insertion d'une mesure de température, pression & humidité dans la table dataBme280
+    // insertion d'une mesure de température, pression ,humidité et point de rosée dans la table dataBme280
     ostringstream sql;
-    sql << "INSERT INTO dataBme280(date, temperature, pression, humidite) VALUES (CURRENT_TIMESTAMP," << fixed << setprecision (2) << capteur.obtenirTemperatureEnC()<< "," <<  fixed << setprecision (2) << capteur.obtenirPression0() << "," <<  fixed << setprecision (2) << capteur.obtenirHumidite()  << ")";
+    sql << "INSERT INTO dataBme280(date, temperature, pression, humidite, pointDeRosee) VALUES (CURRENT_TIMESTAMP," << fixed << setprecision (2) << capteur.obtenirTemperatureEnC()<< "," <<  fixed << setprecision (2) << capteur.obtenirPression0() << "," <<  fixed << setprecision (2) << capteur.obtenirHumidite()  <<  "," <<  fixed << setprecision (2) << capteur.obtenirPointDeRosee()  << ")";
     stmt->execute(sql.str());
 
     delete stmt;
