76d1ad99ad09b5a06969c07211f661eef2c704be
PhilippeSimier <philaure@wanadoo.fr>
2017-05-20 09:35:45 +0200

    update dataLoggers

--- INA219/dataLoggers.cpp
+++ INA219/dataLoggers.cpp
@@ -1,15 +1,15 @@
 /*******************************************************************************************
-* Sujet	: Programme pour enregistrer les valeurs tension courant puissance batterie
-*         dans la table batterie de la base de données MySQL Valeurs
-*  langage   : C++
+*  Sujet	: Programme pour enregistrer les valeurs tension courant puissance batterie
+*         	  dans la table batterie de la base de données MySQL Valeurs
+*  langage      : C++
 *  Auteur 	: SIMIER Philippe
 *  Date   	: Mai 2017
-*  Prérequis : sudo apt-get install libmysqlcppconn-dev
+*  Prérequis    : sudo apt-get install libmysqlcppconn-dev
 *
-*  Compilation :  g++ dataLoggers.cpp -std=c++11 i2c.cpp  ina219.cpp -lmysqlcppconn -o dataLoggers
-*  Execution : Pour executer périodiquement toutes les 10 minutes
-              Ajouter avec crontab
-		en tant que super utilisateur (sudo crontab -e)
+*  Compilation  :  g++ dataLoggers.cpp -std=c++11 i2c.cpp  ina219.cpp -lmysqlcppconn -o dataLoggers
+*  Execution    : Pour executer périodiquement toutes les 10 minutes
+                  Ajouter avec crontab
+		  en tant que super utilisateur (sudo crontab -e)
 ********************************************************************************************/
   //  */10 * * * * /home/pi/Capteurs_I2C/INA219/dataLoggers
 /*******************************************************************************************/
@@ -21,10 +21,22 @@
 #include "cppconn/driver.h"
 #include "cppconn/connection.h"
 #include <cppconn/statement.h>
+#include <time.h>
+
 #include "ina219.h"
 
 using namespace std;
 using namespace sql;
+
+/* fonction pour convertir une chaîne date en timeStamp */
+time_t dateToTimeStamp(std::string dateString){
+
+    struct tm tm;
+    memset(&tm, 0, sizeof(tm));
+
+    strptime(dateString.c_str(), "%Y-%m-%d %T", &tm);
+    return mktime(&tm);
+}
 
 int main(int argc, char* argv[]) {
     Driver* driver;
@@ -32,9 +44,10 @@ int main(int argc, char* argv[]) {
     Statement *stmt;
     ResultSet *res;
     PreparedStatement *pstmt;
-    float energie;
+    float energie = 0.0;
     float u,i,p;
-    float t = 600.0;    // la période d'échantillonage 10 min soit 600s
+    time_t date, now;
+    double t;
 
     ina219 capteur;   // déclaration d'un ina219 à l'adresse par défaut 0x40
     u = capteur.obtenirTension_V();
@@ -53,18 +66,22 @@ int main(int argc, char* argv[]) {
     stmt->execute("USE Valeurs");
 
     //creation de la table batterie si elle n'existe pas
-    stmt->execute("CREATE TABLE IF NOT EXISTS batterie ( id int(11) NOT NULL AUTO_INCREMENT, courantBus float NOT NULL, tensionBus float NOT NULL, puissanceBus float NOT NULL, energie float NOT NULL, date datetime NOT NULL, PRIMARY KEY (id))");
+    stmt->execute("CREATE TABLE IF NOT EXISTS batterie ( id int(11) NOT NULL AUTO_INCREMENT, courantBus float NOT NULL, tensionBus float NOT NULL, puissanceBus float NOT NULL, energie float NOT NULL, date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id))");
 
     // Lecture de la dernière valeur de l'énergie enregistrée
-    res = stmt->executeQuery("SELECT energie FROM batterie ORDER BY id DESC LIMIT 1");
+    res = stmt->executeQuery("SELECT energie , date FROM batterie ORDER BY id DESC LIMIT 1");
     if (res->next()){
+	date = dateToTimeStamp(res->getString("date"));
     	energie = std::stof(res->getString("energie"));
+	time(&now);  // obtenir le  current time
+	t = difftime(now,date) + 3600; // la différence entre maintenant et la date de la dernière mesure
+	energie += p * (float)t / 3.6;   // L'énergie en mWh  3600J = 1Wh  donc 3.6J = 1mWh
     }
-    energie += p * t / 3.6;   // L'énergie en mWh  3600J = 1Wh  donc 3.6J = 1mWh
 
-    // insertion d'une mesure de tensionBus, courantBus , PuissanceBus dans la table batterie
+
+    // insertion des mesures de tensionBus, courantBus , PuissanceBus , energie dans la table batterie
     ostringstream sql;
-    sql << "INSERT INTO batterie(date, tensionBus, courantBus, PuissanceBus, energie) VALUES (CURRENT_TIMESTAMP," << fixed << setprecision (3) << u << "," <<  fixed << setprecision (3) << i << "," <<  fixed << setprecision (3) << p << "," << fixed << setprecision (3) << energie << ")";
+    sql << "INSERT INTO batterie(tensionBus, courantBus, PuissanceBus, energie) VALUES (" << u << "," << i << "," << p << "," << energie << ")";
     stmt->execute(sql.str());
 
     delete stmt;
