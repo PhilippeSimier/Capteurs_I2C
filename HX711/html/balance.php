544b02addc78d13e438ba9d32cf6128b41c3e9d9
philippeSimier <philaure@wanadoo.fr>
2018-06-25 16:44:01 +0200

    update

--- HX711/html/balance.php
+++ HX711/html/balance.php
@@ -4,12 +4,16 @@ require_once('ini/ini.php');
 //------------si des données  sont reçues on les enregistrent dans le fichier configuration.ini ---------
 if( !empty($_POST['envoyer'])){
 
-    $array = array();
+    //  lecture du fichier de configuration
+    $array  = parse_ini_file("/home/pi/Capteurs_I2C/HX711/configuration.ini", true);
+    //  Modification des valeurs pour la section [balance]
     $array['balance'] = array ('scale'  => $_POST['scale'],
                                'offset' => $_POST['offset'],
                                'gain'   => $_POST['gain'],
 			       'unite'  => $_POST['unite'],
                                'precision' => $_POST['precision']);
+
+    //  Ecriture du fichier de configuration modifié
     $ini = new ini ("/home/pi/Capteurs_I2C/HX711/configuration.ini");
     $ini->ajouter_array($array);
     $ini->ecrire(true);
@@ -38,15 +42,56 @@ else
     <title>Configuration Balance</title>
     <!-- Bootstrap CSS version 4.1.1 -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/css/bootstrap.min.css" >
+
+    <!-- Style pour la boite (div id popin) coins arrondis bordure blanche ombre -->
+        <style type="text/css">
+		.popin {
+				background-color: #fff;
+				border-radius: 8px;
+				box-shadow: 0 0 20px #999;
+				padding: 10px;
+				margin: 30px;
+
+		}
+
+		.h1 {
+			font-size: 80px;
+		}
+
+		</style>
+
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script type="text/javascript">
+			function affiche( data ) {               // fonction pour afficher les données reçues
+				console.log(data);               // affichage de data dans la console
+				$('#Weight').text(data.Weight);
+
+			}
+
+			function requete_ajax(){
+				// requete Ajax méthode getJSON
+				$.getJSON(
+					"/cgi-bin/balanceJson", // Le fichier cible côté serveur. data au format Json
+					affiche
+				);
+			}
+
+			$(document).ready(function(){
+
+			    $.getJSON("/cgi-bin/balanceJson", affiche); // affichage des données quand la page est dispo
+				setInterval(requete_ajax, 1200);  // appel de la fonction requete_ajax toutes les 10 secondes
+				//setInterval("requete_ajax()", 1000);  écriture alternative  possible avec une chaîne "requete_ajax()"
+			});
+		</script>
+
 </head>
 <body>
     <div class="row">
-	<div class=" col-md-3"></div>
-    <div class=" col-md-6">
-
-		<h2>Configuration de la balance</h2>
+        <div class="col-md-6">
+            <div class="popin">
+            <h2>Configuration de la balance</h2>
 		<form class="form-horizontal" method="post" action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" name="configuration" >
 
 		<div class="form-group">
@@ -78,11 +123,16 @@ else
                 </div>
 
 
+		<button type="submit" class="btn btn-primary" value="Valider" name="envoyer" > Valider</button>
 
-			<button type="submit" class="btn btn-primary" value="Valider" name="envoyer" > Valider</button>
+            </div>
+            </div>
 
+	    <div class="col-md-6">
+            <div class="popin">
+                <h1 class="h1"><span id="Weight"></span></h1>
+            </div>
+	    </div>
 
-    </div>
-	<div class=" col-md-3"></div>
-    </div>
+        </div>
 </body>
